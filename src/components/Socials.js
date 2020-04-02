e22facc6a4328a38d3e64e2d227e6cbb49f0a8a1
sabicd <daniel.sabic@jaystack.com>
2020-04-02 09:41:51 +0200

    Implement dynamic content creation
    Use external datasource for social links, and job related information

---
+++ src/components/Socials.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SocialIcon from "./SocialIcon";
+import socials from "../constants/socials.json";
+const Socials = () => {
+	return (
+		<>
+			{socials.map(social => (
+				<SocialIcon
+					name={social.name}
+					link={social.link}
+					key={social.name}
+				/>
+			))}
+		</>
+	);
+};
+
+export default Socials;
