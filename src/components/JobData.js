2902fdd8185f19a7e4a9f45a64cb512eed97ee8f
sabicd <daniel.sabic@jaystack.com>
2020-04-07 14:17:48 +0200

    MODIFY: switch from non responsive prototypes (stack) to framer motion component
    Remove stack and Frames, replace them with framer motion divs
    
    Use grid to align items easier

--- src/components/JobData.js
+++ src/components/JobData.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Chip from '@material-ui/core/Chip';
 import job from '../constants/job.json';
 import { makeStyles } from '@material-ui/core/styles';
-import { Frame, Stack } from 'framer';
+import Grid from '@material-ui/core/Grid';
+import { motion } from 'framer';
 const { techsUsed, ...jobData } = job;
 const jobDataKeys = Object.keys(jobData);
+const jobDataParsed = jobDataKeys.map((key) => `${key}: ${jobData[key]}`);
 
+const shuffle = (arr) =>
+  arr
+    .map((a) => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map((a) => a[1]);
+
+const spring = {
+  type: 'spring',
+  damping: 10,
+  stiffness: 150,
+};
 const useStyles = makeStyles((theme) => ({
   techChip: {
     margin: theme.spacing(0.5),
@@ -19,42 +32,50 @@ const useStyles = makeStyles((theme) => ({
 const JobTitle = () => {
   const classes = useStyles();
 
+  const [jobDataState, setJobData] = useState(jobDataParsed);
+
+  useEffect(() => {
+    setTimeout(() => setJobData(shuffle(jobDataState)), 3000);
+  }, [jobDataState]);
+
   return (
     <>
-      {jobDataKeys.map((jobKey) => (
-        <Chip
-          key={jobKey}
-          label={`${jobKey}: ${job[jobKey]}`}
-          className={classes.jobChip}
-        />
-      ))}
-      <Stack
-        height={40}
-        width={60 * techsUsed.length}
-        gap={20}
-        position={'relative'}
-        distribution='center'
-        direction={'horizontal'}>
-        {techsUsed.map((tech) => (
-          <Frame
-            height={35}
-            dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-            drag={true}
-            width={tech.length < 5 ? 35 : 45 + tech.length}
-            whileHover={{ scale: 0.95 }}
-            background={null}
-            dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
-            key={tech}>
-            <Chip
-              size='small'
-              key={tech}
-              label={tech}
-              clickable
-              className={classes.techChip}
-            />
-          </Frame>
-        ))}
-      </Stack>
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        justify='center'
+        key='jobTitleGrid'>
+        <Grid item container key='jobTitleContentHolder'>
+          {jobDataState.map((element) => (
+            <motion.div layoutTransition={spring} key={element}>
+              <Chip key={element} label={element} className={classes.jobChip} />
+            </motion.div>
+          ))}
+        </Grid>
+
+        <Grid item container key='techs'>
+          {techsUsed.map((tech) => (
+            <motion.div
+              height={35}
+              dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+              drag={true}
+              width={tech.length < 5 ? 35 : 45 + tech.length}
+              whileHover={{ scale: 0.95 }}
+              background={null}
+              dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
+              key={tech}>
+              <Chip
+                size='small'
+                key={tech}
+                label={tech}
+                clickable
+                className={classes.techChip}
+              />
+            </motion.div>
+          ))}
+        </Grid>
+      </Grid>
     </>
   );
 };
