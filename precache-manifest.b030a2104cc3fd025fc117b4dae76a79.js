631c48e05c99f293b777f1d9b4a614514a10f477
sabicd <daniel.sabic@jaystack.com>
2021-03-12 12:53:41 +0100

    Updates

---
+++ precache-manifest.b030a2104cc3fd025fc117b4dae76a79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02196d75b59a53e69754bb7137586e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73069ff2dd0eb4f2a9bd",
+    "url": "/static/css/main.b8df9b81.chunk.css"
+  },
+  {
+    "revision": "241e4d5811893648f989",
+    "url": "/static/js/2.22d48860.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.22d48860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73069ff2dd0eb4f2a9bd",
+    "url": "/static/js/main.54b30bee.chunk.js"
+  },
+  {
+    "revision": "7cd2f961a92ccb3c93ab",
+    "url": "/static/js/runtime-main.fb23c3bf.js"
+  },
+  {
+    "revision": "d487911eda1f82c3f48380a599e4967a",
+    "url": "/static/media/ProfilePicture.d487911e.jpeg"
+  }
+]);
